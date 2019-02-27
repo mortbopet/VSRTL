45dd2a15bcc3cfc7059c33e0eaa42e6188910681
Morten Borup Petersen <morten_bp@live.dk>
2019-02-27 21:00:32 +0000

    Add binary signal collator component

---
+++ core/vsrtl_collator.h
@@ -0,0 +1,42 @@
+#ifndef VSRTL_COLLATOR_H
+#define VSRTL_COLLATOR_H
+
+#include "vsrtl_component.h"
+
+namespace vsrtl {
+
+
+/**
+ * @brief The Collator class
+ * Collates binary signals into a compound signal
+ * ie:
+ *           _____
+ * a[0:0] -> |   |
+ * b[0:0] -> |   | -> [3:0] a&b&c&d
+ * c[0:0] -> |   |
+ * d[0:0] -> |___|
+ */
+class Collator : public Component {
+public:
+    Collator(std::string name, unsigned int width) : Component(name), m_width(width) {
+        in = this->createInputPorts("in", width, 1);
+        out.setWidth(width);
+
+        out << [=] {
+            VSRTL_VT_U value = 0;
+            for(int i = 0; i < width; i++){
+                value |= static_cast<bool>(*in[i]) << i;
+            }
+            return value;
+        };
+    }
+    OUTPUTPORT(out);
+    INPUTPORTS(in);
+
+protected:
+    unsigned int m_width;
+};
+
+}
+
+#endif // VSRTL_COLLATOR_H
