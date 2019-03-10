6f5b4ddf34b283326812d5b1651dce6e27bd4b9d
Morten Borup Petersen <morten_bp@live.dk>
2019-03-10 11:45:34 +0000

    Add random number generator unit test

---
+++ test/tst_rannumgen.cpp
@@ -0,0 +1,33 @@
+#include <QtTest/QTest>
+
+#include "vsrtl_rannumgen.h"
+
+using namespace vsrtl;
+
+class tst_rannumgen : public QObject {
+    Q_OBJECT private slots : void functionalTest();
+};
+
+VSRTL_VT_U xorshift(VSRTL_VT_U x) {
+    x ^= x << 13;
+    x ^= x >> 17;
+    x ^= x << 5;
+    return x;
+}
+
+void tst_rannumgen::functionalTest() {
+    vsrtl::RanNumGen a;
+    a.verifyAndInitialize();
+
+    VSRTL_VT_U seed = 0x13fb27a3;
+    // Clock the circuit n times
+    for (int i = 0; i < 10; i++) {
+        a.clock();
+        VSRTL_VT_U circuit_val = a.rngResReg->out.template value<VSRTL_VT_U>();
+        QVERIFY(circuit_val == seed);
+        seed = xorshift(seed);
+    }
+}
+
+QTEST_APPLESS_MAIN(tst_rannumgen)
+#include "tst_rannumgen.moc"
