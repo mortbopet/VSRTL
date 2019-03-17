cc1a1de6d72a3a71d4346c6b45c0fb979049c3a1
Morten Borup Petersen <morten_bp@live.dk>
2019-03-17 13:30:43 +0000

    Add NetlistView class for handling view context menu requests
    The NetlistView wil be able to access item-specific functions, by being
    instantiated with the same template parameter as the respective model.

---
+++ graphics/vsrtl_netlistview.h
@@ -0,0 +1,39 @@
+#ifndef VSRTL_NETLISTVIEW_H
+#define VSRTL_NETLISTVIEW_H
+
+#include <QMenu>
+#include <QTreeView>
+
+namespace vsrtl {
+
+/**
+ * Template parameter is expected to be a subclass of a TreeItem
+ */
+template <typename T>
+class NetlistView : public QTreeView {
+public:
+    NetlistView(QWidget* parent) : QTreeView(parent) {
+        setContextMenuPolicy(Qt::CustomContextMenu);
+        connect(this, &QTreeView::customContextMenuRequested, this, &NetlistView<T>::contextMenuRequest);
+    }
+
+private slots:
+    void contextMenuRequest(const QPoint& pos) {
+        QModelIndex index = indexAt(pos);
+        if (index.isValid()) {
+            /* The TreeItem at the given index will be queried for its available actions (both TreeItem actions and
+             * derived class actions). These actions are then presented in the context menu */
+            T* item = static_cast<T*>(index.internalPointer());
+
+            QMenu menu;
+            const auto& actions = item->getActions();
+            if (actions.size() != 0) {
+                menu.addActions(item->getActions());
+                menu.exec(viewport()->mapToGlobal(pos));
+            }
+        }
+    }
+};
+
+}  // namespace vsrtl
+#endif  // VSRTL_NETLISTVIEW_H
