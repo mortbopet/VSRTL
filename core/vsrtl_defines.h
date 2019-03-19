3f99dfb61b095520ed619d0e5526e2c1a33f59b3
Morten Borup Petersen <mortbopet@users.noreply.github.com>
2019-03-19 16:46:28 +0000

    Add static assertions for VSRTL base data types

--- core/vsrtl_defines.h
+++ core/vsrtl_defines.h
@@ -7,6 +7,8 @@ namespace vsrtl {
 using VSRTL_VT_U = unsigned int;
 using VSRTL_VT_S = int;
 static_assert(sizeof(VSRTL_VT_S) == sizeof(VSRTL_VT_U), "Base value types must be equal in size");
-
+static_assert(std::is_unsigned<VSRTL_VT_U>::value, "VSRTL_VT_U must be an unsigned data type");
+static_assert(std::is_signed<VSRTL_VT_S>::value, "VSRTL_VT_S must be a signed data type");
+  
 }  // namespace vsrtl
 #endif  // VSRTLDEFINES_H
