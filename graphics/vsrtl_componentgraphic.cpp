79d4cd174110f1d97d033c5ae170d69689a8eca6
Morten Borup Petersen <mortbopet@users.noreply.github.com>
2019-03-20 10:16:17 +0000

    Remove unused code in vsrtl_componentgraphic.cpp
    This code has been moved to https://github.com/mortbopet/VSRTL/blob/master/graphics/vsrtl_placeroute.cpp

--- graphics/vsrtl_componentgraphic.cpp
+++ graphics/vsrtl_componentgraphic.cpp
@@ -97,33 +97,6 @@ void ComponentGraphic::createSubcomponents() {
     }
 }
 
-/**
- * @brief orderSubcomponents
- * Subcomponent ordering is based on a topological sort algorithm.
- * This algorithm is applicable for DAG's (Directed Acyclical Graph's). Naturally, digital circuits does not fulfull the
- * requirement for being a DAG. However, if Registers are seen as having either their input or output disregarded as an
- * edge in a DAG, a DAG can be created from a digital circuit, if only outputs are considered.
- * Having a topological sorting of the components, rows- and columns can
- * be generated for each depth in the topological sort, wherein the components are finally layed out.
- * @param parent
- */
-
-namespace {
-void orderSubcomponentsUtil(Component* c, std::map<Component*, bool>& visited, std::deque<Component*>& stack) {
-    visited[c] = true;
-
-    for (const auto& cc : c->getOutputComponents()) {
-        // The find call ensures that the output components are inside this subcomponent. OutputComponents are "parent"
-        // agnostic, and has no notion of enclosing components.
-        if (visited.find(cc) != visited.end() && !visited[cc]) {
-            orderSubcomponentsUtil(cc, visited, stack);
-        }
-    }
-    stack.push_front(c);
-}
-
-}  // namespace
-
 template <typename K, typename V>
 K reverseLookup(const std::map<K, V>& m, const V& v) {
     for (const auto& i : m) {
