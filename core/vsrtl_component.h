9e5287f16d396207802d4bd453d2e401611f4011
Morten Borup Petersen <morten_bp@live.dk>
2019-03-30 14:44:25 +0000

    Move various vsrtl::Component functions to header
    This fixes a bunch of linker errors which occurs by using TypeInfo on a
    polymorphic class.

--- core/vsrtl_component.h
+++ core/vsrtl_component.h
@@ -34,7 +34,6 @@ namespace vsrtl {
 class Component : public Base {
 public:
     Component(std::string displayName, Component* parent) : m_displayName(displayName), m_parent(parent) {}
-    virtual ~Component() {}
 
     /**
      * @brief getBaseType
@@ -46,7 +45,17 @@ public:
 
     virtual std::type_index getTypeId() const { return std::type_index(typeid(Component)); }
     virtual bool isRegister() const { return false; }
-    virtual void resetPropagation();
+    virtual void resetPropagation() {
+        if (m_propagationState == PropagationState::unpropagated) {
+            // Constants (components with no inputs) are always propagated
+        } else {
+            m_propagationState = PropagationState::unpropagated;
+            for (const auto& i : m_inputports)
+                i->resetPropagation();
+            for (const auto& o : m_outputports)
+                o->resetPropagation();
+        }
+    }
     bool isPropagated() const { return m_propagationState == PropagationState::propagated; }
 
     /**
@@ -62,9 +71,96 @@ public:
     std::vector<Port*> createInputPorts(std::string name, unsigned int n, unsigned int width = 0);
     std::vector<Port*> createOutputPorts(std::string name, unsigned int n, unsigned int width = 0);
 
-    void propagateComponent(std::vector<Port*>& propagationStack);
-    void initialize();
-    void verifyComponent() const;
+    void propagateComponent(std::vector<Port*>& propagationStack) {
+        // Component has already been propagated
+        if (m_propagationState == PropagationState::propagated)
+            return;
+
+        if (isRegister()) {
+            // Registers are implicitely clocked by calling propagate() on its output ports.
+            /** @remark register <must> be saved before propagateComponent reaches the register ! */
+            m_propagationState = PropagationState::propagated;
+            for (const auto& s : m_outputports) {
+                s->propagate(propagationStack);
+            }
+        } else {
+            // All sequential logic must have their inputs propagated before they themselves can propagate. If this is
+            // not the case, the function will return. Iff the circuit is correctly connected, this component will at a
+            // later point be visited, given that the input port which is currently not yet propagated, will become
+            // propagated at some point, signalling its connected components to propagate.
+            for (const auto& input : m_inputports) {
+                if (!input->isPropagated())
+                    return;
+            }
+
+            for (const auto& sc : m_subcomponents)
+                sc->propagateComponent(propagationStack);
+
+            // At this point, all input ports are assured to be propagated. In this case, it is safe to propagate
+            // the outputs of the component.
+            for (const auto& s : m_outputports) {
+                s->propagate(propagationStack);
+            }
+            m_propagationState = PropagationState::propagated;
+
+            // if any internal values have changed...
+            // @todo: implement granular change signal emission
+            changed.Emit();
+        }
+
+        // Signal all connected components of the current component to propagate
+        for (const auto& out : m_outputports) {
+            for (const auto& in : out->getOutputPorts()) {
+                // With the input port of the connected component propagated, the parent component may be propagated.
+                // This will succeed if all input components to the parent component has been propagated.
+                in->getParent()->propagateComponent(propagationStack);
+
+                // To facilitate output -> output connections, we need to trigger propagation in the output's parent
+                // aswell
+                /**
+                 * IN   IN   OUT  OUT
+                 *   _____________
+                 *  |    _____   |
+                 *  |   |    |   |
+                 *  |   |   ->--->
+                 *  |   |____|   |
+                 *  |____________|
+                 *
+                 */
+                for (const auto& inout : in->getOutputPorts())
+                    inout->getParent()->propagateComponent(propagationStack);
+            }
+        }
+    }
+    void initialize() {
+        if (m_inputports.size() == 0 && !hasSubcomponents()) {
+            // Component has no input ports - ie. component is a constant. propagate all output ports and set component
+            // as propagated.
+            for (const auto& p : m_outputports)
+                p->propagateConstant();
+            m_propagationState = PropagationState::propagated;
+        }
+    }
+    void verifyComponent() const {
+        for (const auto& ip : m_inputports) {
+            if (!ip->isConnected()) {
+                throw std::runtime_error("A component has unconnected inputs");
+            }
+            if (ip->getWidth() == 0) {
+                throw std::runtime_error(
+                    "A port did not have its width set. Parent component of port should set port width in its "
+                    "constructor");
+            }
+        }
+
+        for (const auto& op : m_outputports) {
+            if (op->getWidth() == 0) {
+                throw std::runtime_error(
+                    "A port did not have its width set. Parent component of port should set port width in its "
+                    "constructor");
+            }
+        }
+    }
     bool hasSubcomponents() const { return m_subcomponents.size() != 0; }
 
     /**
@@ -99,7 +195,14 @@ protected:
     Port& createPort(std::string name, std::vector<std::unique_ptr<Port>>& container, unsigned int width);
     std::vector<Port*> createPorts(std::string name, std::vector<std::unique_ptr<Port>>& container, unsigned int n,
                                    unsigned int width);
-    void getComponentGraph(std::map<Component*, std::vector<Component*>>& componentGraph);
+    void getComponentGraph(std::map<Component*, std::vector<Component*>>& componentGraph) {
+        // Register adjacent components (child components) in the graph, and add subcomponents to graph
+        componentGraph[this];
+        for (const auto& c : m_subcomponents) {
+            componentGraph[this].push_back(c.get());
+            c->getComponentGraph(componentGraph);
+        }
+    }
 
     mutable bool m_isVerifiedAndInitialized = false;
     std::string m_displayName;
