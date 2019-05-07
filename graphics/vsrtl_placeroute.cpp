0832b5f53134ed056d253f0324306054caad8c5d
Morten Borup Petersen <morten_bp@live.dk>
2019-05-07 20:28:34 +0100

    Add locked group to KL algorithm
    By this, all nodes which have previously been swapped between the two
    subsets are no longer considered for further iterations.

--- graphics/vsrtl_placeroute.cpp
+++ graphics/vsrtl_placeroute.cpp
@@ -13,7 +13,7 @@ namespace vsrtl {
 namespace pr {
 
 template <typename T>
-void largestRunningSum(const T& c, int& i_max, int& sum_max) {
+void largestRunningSum(const T& c, unsigned int& i_max, int& sum_max) {
     int sum = c[0];
     sum_max = sum;
     i_max = 0;
@@ -62,9 +62,9 @@ int KLdValue(Component* node, const std::set<Component*>& A, const std::set<Comp
 
 template <typename T>
 void splitContainer(const T& in, T& A, T& B) {
-    int i = 0;
+    unsigned int i = 0;
     for (const auto& it : in) {
-        if (i % 2 == 0) {
+        if (i < (in.size() / 2)) {
             A.emplace(it);
         } else {
             B.emplace(it);
@@ -79,8 +79,16 @@ PartitionedGraph KernighanLin(std::set<Component*>& graph) {
     std::set<Component*> A, B;
     splitContainer(graph, A, B);
 
+    // Initially all nodes may be selected and the locked nodes of each set are empty
+    std::set<Component*> A_locked, B_locked;
+
     int g_max = INT_MIN;
     do {
+        // A maximum number of swaps has been performed if either of the locked sets are equal to half of the original
+        // set size. Note that this adds support of when graph is an uneven number of nodes.
+        if (A_locked.size() == graph.size() / 2) {
+            break;
+        }
         // Calculate D value for each node in A, B,
         std::map<Component*, int> D;
         for (const auto& c : graph) {
@@ -91,30 +99,33 @@ PartitionedGraph KernighanLin(std::set<Component*>& graph) {
         std::vector<Component*> av, bv;
         std::vector<int> gv;
 
-        // While partitioning the graph, the number of nodes which are to be considered in each pass shrinks with each
-        // iteration. Reflect these changes in sets A_pass, B_pass
-        std::set<Component*> A_pass = A;
-        std::set<Component*> B_pass = B;
-        for (int i = 0; i < graph.size() / 2; i++) {
+        // The nodes to be considered in this pass are all nodes of A and B which have not previously been moved
+        std::set<Component*> A_pass;
+        std::set<Component*> B_pass;
+        std::set_difference(A.begin(), A.end(), A_locked.begin(), A_locked.end(),
+                            std::inserter(A_pass, A_pass.begin()));
+        std::set_difference(B.begin(), B.end(), B_locked.begin(), B_locked.end(),
+                            std::inserter(B_pass, B_pass.begin()));
+        const size_t passSize = (A_pass.size() + B_pass.size());
+        for (unsigned int i = 0; i < passSize / 2; i++) {
             // find a from A and b from B, such that g = D[a] + D[b] - 2*c(a, b) is maximal
             Component* a = nullptr;
             Component* b = nullptr;
-            int d_max = INT_MIN;
-            int g = d_max;
+            int g_pass_max = INT_MIN;
             for (const auto& a_it : A_pass) {
                 for (const auto& b_it : B_pass) {
                     // Cost between two nodes is equal to the D-value between the two nodes.
-                    g = D[a_it] + D[b_it] - 2 * KLdValue(a_it, {a_it}, {b_it});
-                    if (g > d_max) {
+                    const int g = D[a_it] + D[b_it] - 2 * KLdValue(a_it, {a_it}, {b_it});
+                    if (g > g_pass_max) {
                         a = a_it;
                         b = b_it;
-                        d_max = g;
+                        g_pass_max = g;
                     }
                 }
             }
             Q_ASSERT(a != nullptr && b != nullptr);
             // a and b which maximizes g has been found. Add values to gv, av, bv
-            gv.push_back(g);
+            gv.push_back(g_pass_max);
             av.push_back(a);
             bv.push_back(b);
             // remove a and b from further consideration in this pass.
@@ -131,15 +142,17 @@ PartitionedGraph KernighanLin(std::set<Component*>& graph) {
         }
 
         // find index i which maximizes g_max, the sum of gv[0],...,gv[i]
-        int i_max;
+        unsigned int i_max;
         largestRunningSum(gv, i_max, g_max);
         if (g_max > 0) {
-            // Exchange av[0],av[1],...,av[i] with bv[0],bv[1],...,bv[i]
-            for (int i = 0; i <= i_max; i++) {
+            // Permanently exchange av[0],av[1],...,av[i] with bv[0],bv[1],...,bv[i]
+            for (size_t i = 0; i <= i_max; i++) {
                 A.erase(av[i]);
                 A.emplace(bv[i]);
                 B.erase(bv[i]);
                 B.emplace(av[i]);
+                A_locked.emplace(bv[i]);
+                B_locked.emplace(av[i]);
             }
         }
     } while (g_max > 0);
@@ -217,7 +230,6 @@ enum class CutlineDirection { Alternating, Repeating };
 class PartitioningTree : public BinTree<Component*, PartitioningTree> {
 public:
     Direction cutlinedir;
-    const int rectpadding = 2;
     QRect cachedRect;
 
     void place(const QPoint offset) {
@@ -358,8 +370,8 @@ void MinCutPlacement(const std::vector<ComponentGraphic*>& components) {
     QRect circuitRect = rootPartitionNode.rect();
     // add padding around the edges of the circuit rectangle. padding is added in the positive x- and y directions to
     // maintain the top-left point of the circuit rectangle in (0,0)
-    const int padding = 0;
-    circuitRect.adjusted(0, 0, padding, padding);
+    const int padding = 4;
+    circuitRect.adjust(0, 0, padding, padding);
 
     // Place the circuit with the center of the circuit rectangle as the initial offset point
     rootPartitionNode.place(circuitRect.center());
