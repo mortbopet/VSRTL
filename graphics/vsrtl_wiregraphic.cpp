5bddf51cfb49dad97585bdf06397d9deb50d044b
Morten Borup Petersen <morten_bp@live.dk>
2019-10-10 09:55:48 +0200

    Implement selection shape for wire segments

--- graphics/vsrtl_wiregraphic.cpp
+++ graphics/vsrtl_wiregraphic.cpp
@@ -46,15 +46,34 @@ QLineF WireSegment::getLine() const {
     return QLineF(startPos, endPos);
 }
 
-QPainterPath WireSegment::shape() const {
-    // @todo: expand the shape a bit out from the wire, for easier clicking
+/**
+ * @brief expandLine
+ * Given a line, returns a box of width 2xsideWidth around the line
+ */
+QPolygonF expandLine(const QLineF line, const qreal sideWidth) {
+    const auto normp1 = line.normalVector();
+    const auto normp2 = normp1.translated(line.p2());
+    auto normp1inv = normp1;
+    auto normp2inv = normp2;
+    normp1inv.setAngle(normp1inv.angle() + 180);
+    normp2inv.setAngle(normp2inv.angle() + 180);
 
+    const auto lineWidthParam = qreal(sideWidth) / line.length();
+
+    const QPointF rp1 = normp1.pointAt(lineWidthParam);
+    const QPointF rp2 = normp2.pointAt(lineWidthParam);
+    const QPointF rp3 = normp2inv.pointAt(lineWidthParam);
+    const QPointF rp4 = normp1inv.pointAt(lineWidthParam);
+
+    return QPolygonF({rp1, rp2, rp3, rp4});
+}
+
+QPainterPath WireSegment::shape() const {
     if (m_start == nullptr || m_end == nullptr)
         return QPainterPath();
 
-    const auto line = getLine();
-    QPainterPath path(line.p1());
-    path.lineTo(line.p2());
+    QPainterPath path;
+    path.addPolygon(expandLine(getLine(), WIRE_WIDTH));
     return path;
 }
 
@@ -82,7 +101,13 @@ void WireSegment::paint(QPainter* painter, const QStyleOptionGraphicsItem*, QWid
 
 void WireSegment::contextMenuEvent(QGraphicsSceneContextMenuEvent* event) {
     // Todo: allow adding points. Should possibly just be done by a click on the wire
+    QMenu menu;
+    auto a = QAction("abc");
+    menu.addAction(&a);
+    menu.exec(event->screenPos());
 }
+
+void WireSegment::mousePressEvent(QGraphicsSceneMouseEvent* event) {}
 
 // --------------------------------------------------------------------------------
 
