b73b543e97d1892e6e555b49a37206429a907df0
Morten Borup Petersen <morten_bp@live.dk>
2019-10-17 16:58:10 +0200

    Extend the VSRTL Enum class

--- core/vsrtl_enum.h
+++ core/vsrtl_enum.h
@@ -4,15 +4,32 @@
 #include "vsrtl_binutils.h"
 
 namespace vsrtl {
-/** Enum
+
+#define FIRST_ARG_(N, ...) N
+
+/**
+ * @brief The Enum struct
  * Create an enum with an associated width() function, returning the required
- * number of bits to encode the number of enums
+ * number of bits to encode the number of enums.
+ * @todo: a static assert stating that enum start at 0, and are spaced by 1 (ie. non-user assigned)
+ * @todo: an iterater over the enum values
  */
-#define Enum(name, ...)                                                             \
-    struct name {                                                                   \
-        enum _##name{__VA_ARGS__, _COUNT};                                          \
-        static constexpr unsigned int width() { return ceillog2(_##name::_COUNT); } \
-    }
+#define Enum(name, ...)                                                                               \
+    class name {                                                                                      \
+    public:                                                                                           \
+        enum _##name{__VA_ARGS__, _COUNT};                                                            \
+        static constexpr unsigned int width() { return ceillog2(static_cast<int>(_##name::_COUNT)); } \
+        static bool inRange(int v) { return v >= FIRST_ARG_(__VA_ARGS__) && v < _COUNT; }             \
+        static constexpr _##name begin() { return FIRST_ARG_(__VA_ARGS__); }                          \
+        static constexpr _##name count() { return static_cast<_##name>(_COUNT); }                     \
+        static const name& get() {                                                                    \
+            static name e;                                                                            \
+            return e;                                                                                 \
+        }                                                                                             \
+                                                                                                      \
+    private:                                                                                          \
+        name(){};                                                                                     \
+    };
 
 #define Switch(signal, enumname) switch (signal.value<unsigned int>())
 
