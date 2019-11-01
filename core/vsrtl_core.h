15dc7583c227c8e02be8be345ce69fcbdbdb7a52
Morten Borup Petersen <morten_bp@live.dk>
2019-11-01 09:12:32 +0100

    Move MemoryTester to test file

--- core/vsrtl_core.h
+++ core/vsrtl_core.h
@@ -1,8 +1,12 @@
 #ifndef VSRTL_CORE_H
 #define VSRTL_CORE_H
 
+#include "vsrtl_adder.h"
 #include "vsrtl_alu.h"
+#include "vsrtl_comparator.h"
+#include "vsrtl_component.h"
 #include "vsrtl_constant.h"
+#include "vsrtl_design.h"
 #include "vsrtl_logicgate.h"
 #include "vsrtl_memory.h"
 #include "vsrtl_multiplexer.h"
--- test/tst_memory.cpp
+++ test/tst_memory.cpp
@@ -1,13 +1,83 @@
 #include <QtTest/QTest>
 
-#include "vsrtl_memorytester.h"
+#include "vsrtl_binutils.h"
+#include "vsrtl_core.h"
+
+namespace vsrtl {
+/**
+ * @brief The ContinuousIncrement design
+ * A memory is instantiated wherein the following operation is executed:
+ *      mem[i] = mem[i-1] + 1;
+ */
+class ContinuousIncrement : public Design {
+public:
+    ContinuousIncrement() : Design("Registerfile Tester") {
+        idx_reg->out >> mem->addr;
+        mem->data_out >> acc_reg->in;
+        inc_adder->out >> mem->data_in;
+        1 >> idx_adder->op1;
+        idx_reg->out >> idx_adder->op2;
+        1 >> inc_adder->op1;
+        acc_reg->out >> inc_adder->op2;
+
+        wr_en_reg->out >> idx_next_mux->select;
+        idx_adder->out >> *idx_next_mux->ins[0];
+        idx_reg->out >> *idx_next_mux->ins[1];
+
+        idx_reg->out >> comp->op1;
+        (regSize - 1) >> comp->op2;
+        idx_next_mux->out >> idx_reg->in;
+
+        // Write/Read state
+        wr_en_mux->out >> wr_en_reg->in;
+        wr_en_reg->out >> wr_en_mux->select;
+        wr_en_reg->out >> mem->wr_en;
+        0 >> *wr_en_mux->ins[1];
+        1 >> *wr_en_mux->ins[0];
+    }
+    static constexpr unsigned int regSize = 32;
+
+    // Create objects
+    SUBCOMPONENT(mem, TYPE(Memory<regSize, regSize>));
+
+    SUBCOMPONENT(idx_adder, Adder<regSize>);
+    SUBCOMPONENT(inc_adder, Adder<regSize>);
+    SUBCOMPONENT(wr_en_mux, TYPE(Multiplexer<2, 1>));
+    SUBCOMPONENT(idx_next_mux, TYPE(Multiplexer<2, regSize>));
+
+    SUBCOMPONENT(comp, Eq<regSize>);
+
+    SUBCOMPONENT(wr_en_reg, Register<1>);
+    SUBCOMPONENT(idx_reg, Register<regSize>);
+    SUBCOMPONENT(acc_reg, Register<regSize>);
+};
+
+class WriteSameIdx : public Design {
+public:
+    WriteSameIdx() : Design("Registerfile Tester") {
+        mem->data_out >> inc_adder->op1;
+        1 >> inc_adder->op2;
+        inc_adder->out >> mem->data_in;
+        1 >> mem->wr_en;
+        0x0 >> mem->addr;
+    }
+    static constexpr unsigned int regs = 32;
+    static constexpr unsigned int regWidth = CHAR_BIT * sizeof(VSRTL_VT_U);
+
+    // Create objects
+    SUBCOMPONENT(mem, TYPE(Memory<ceillog2(regs), regWidth>));
+    SUBCOMPONENT(inc_adder, Adder<regWidth>);
+};
+
+}  // namespace vsrtl
 
 class tst_memory : public QObject {
     Q_OBJECT private slots : void functionalTest();
+    void repeatedWriteSameIdx();
 };
 
 void tst_memory::functionalTest() {
-    vsrtl::MemoryTester a;
+    vsrtl::ContinuousIncrement a;
 
     a.verifyAndInitialize();
 
@@ -17,6 +87,19 @@ void tst_memory::functionalTest() {
         a.clock();
 
     QVERIFY(a.mem->data_out.template value<uint32_t>() == n / 2);
+}
+
+void tst_memory::repeatedWriteSameIdx() {
+    vsrtl::WriteSameIdx a;
+
+    a.verifyAndInitialize();
+
+    const int n = 10;
+    // Clock the circuit n times
+    for (int i = 0; i < n; i++) {
+        QVERIFY(a.mem->data_out.template value<uint32_t>() == i);
+        a.clock();
+    }
 }
 
 QTEST_APPLESS_MAIN(tst_memory)
