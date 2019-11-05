1636fdbaa8afcc1a9cd4ff2d3c3f9550294ba0da
Morten Borup Petersen <morten_bp@live.dk>
2019-11-05 19:53:54 +0100

    Do not traverse all scene objects when locating ports
    this results in a major speedup in scene initialization.

--- graphics/vsrtl_wiregraphic.cpp
+++ graphics/vsrtl_wiregraphic.cpp
@@ -489,16 +489,8 @@ void WireGraphic::createRectilinearSegments(PortPoint* start, PortPoint* end) {
  * attached input- and output ports
  */
 void WireGraphic::postSceneConstructionInitialize1() {
-    for (const auto& item : scene()->items()) {
-        PortGraphic* portItem = dynamic_cast<PortGraphic*>(item);
-        if (portItem) {
-            if (std::find(m_toPorts.begin(), m_toPorts.end(), portItem->getPort()) != m_toPorts.end()) {
-                m_toGraphicPorts.push_back(portItem);
-            }
-        }
-        if (m_toGraphicPorts.size() == m_toPorts.size()) {
-            break;
-        }
+    for (const auto& toPort : m_toPorts) {
+        m_toGraphicPorts.push_back(getGraphic<PortGraphic*>(toPort));
     }
 
     // Assert that all ports were found in the scene
