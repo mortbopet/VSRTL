1f0fb0c013fe38e1a3a23fc9fe0510432570f971
Morten Borup Petersen <morten_bp@live.dk>
2019-12-31 01:01:34 +0100

    Add unit test for RV32IM simulators
    This test suite will, using the RISC-V toolchain, assemble and
    execute RISC-V test programs on the implemented RISC-V ISA
    simulator.
    
    For now, the test suite has some quite heavy assumptions
    (no floating point, entry point must be 0x0, etc.).

--- test/tst_riscv.cpp
+++ test/tst_riscv.cpp
@@ -1,46 +1,156 @@
+#include <QDir>
+#include <QProcess>
+#include <QStringList>
 #include <QtTest/QTest>
 
 #include "RISC-V/riscv_ss.h"
 
+#ifndef VSRTL_RISCV_TEST_DIR
+static_assert(false, "VSRTL_RISCV_TEST_DIR must be defined");
+#endif
+
+/** RISC-V test suite
+ *
+ * For now, the following assumptions are made:
+ * - When compiling, it is assumed that the entry point address is 0x0
+ * - No .data segment is contained within the resulting .ELF file
+ * As such, we directly copy the .text segment into the simulator memory and execute the test.
+ */
+
+using namespace vsrtl::RISCV;
+
+// Compilation tools & directories
+const QString s_testdir = VSRTL_RISCV_TEST_DIR;
+const QString s_outdir = s_testdir + QDir::separator() + "build";
+const QString s_assembler = "riscv64-linux-gnu-as";
+const QString s_objcopy = "riscv64-linux-gnu-objcopy";
+const QString s_linkerScript = "rvtest.ld";
+
+// Ecall status codes
+static constexpr unsigned s_success = 42;
+static constexpr unsigned s_fail = 0;
+
+// Test status register
+static constexpr unsigned s_statusreg = 3;  // Current test stored in the gp(3) register
+static constexpr unsigned s_ecallreg = 10;  // a0
+
+// Maximum cycle count
+static constexpr unsigned s_maxCycles = 10000;
+
+// Tests which contains instructions or assembler directives not yet supported
+const auto s_excludedTests = {"f", "ldst", "move", "recoding"};
+
+QString compileTestFile(const QString& testfile) {
+    QProcess exec;
+
+    const QString outElf = s_outdir + QDir::separator() + testfile + ".out";
+    const QString outBin = s_outdir + QDir::separator() + testfile + ".bin";
+
+    if (!QDir(s_outdir).exists()) {
+        QDir().mkdir(s_outdir);
+    }
+
+    // Build
+    bool error = exec.execute(s_assembler, {"-march=rv32im", s_testdir + QDir::separator() + testfile, "-o", outElf});
+
+    // Extract .text segment
+    error = exec.execute(s_objcopy, {"-O", "binary", "--only-section=.text", outElf, outBin});
+
+    return error ? QString() : outBin;
+}
+
 class tst_RISCV : public QObject {
     Q_OBJECT
 
+private:
+    void loadBinaryToSimulator(const QString& binFile);
+    bool skipTest(const QString& test);
+    QString executeSimulator();
+
+    QString m_currentTest;
+    std::unique_ptr<SingleCycleRISCV> m_design;
+
 private slots:
-    void regIncBrJump();
+    void runTests();
 };
 
-void tst_RISCV::regIncBrJump() {
-    /**
-  start:
-    li a0 0
-    li a1 10
-  loop:
-    addi a0 a0 1
-    bne a0 a1 loop
-    j start
-    */
-    std::vector<unsigned> program = {0x00000513, 0x00a00593, 0x00150513, 0xfeb51ee3, 0xff1ff06f};
-    vsrtl::RISCV::SingleCycleRISCV design;
-
-    design.instr_mem->addInitializationMemory(0x0, program.data(), program.size());
-    design.verifyAndInitialize();
-
-    int cycles = 0;
-    int jumpCount = 0;
-    const int jumpTarget = 3;
-    const int maxCycles = 100;
-    const int jumpInstrAddr = 16;
-    while (cycles < maxCycles) {
-        if (design.pc_reg->getOut()->uValue() == jumpInstrAddr) {
-            jumpCount++;
+bool tst_RISCV::skipTest(const QString& test) {
+    for (const auto& t : s_excludedTests) {
+        if (test.startsWith(t)) {
+            return true;
         }
-        if (jumpCount == jumpTarget)
-            break;
+    }
+    return false;
+}
 
-        design.clock();
+void tst_RISCV::loadBinaryToSimulator(const QString& binFile) {
+    // Read test file
+    QFile testFile(binFile);
+    if (!testFile.open(QIODevice::ReadOnly)) {
+        QString err = "Test: '" + m_currentTest + "' failed: Could not read compiled test file.";
+        QFAIL(err.toStdString().c_str());
+    }
+    QByteArray programByteArray = testFile.readAll();
+    std::vector<unsigned char> program(programByteArray.begin(), programByteArray.end());
+
+    // Load program into simulator
+    m_design->instr_mem->addInitializationMemory(0x0, program.data(), program.size());
+}
+
+QString tst_RISCV::executeSimulator() {
+    bool stop = false;
+    bool maxCyclesReached = false;
+    unsigned cycles = 0;
+    do {
+        m_design->clock();
         cycles++;
-        if (cycles >= maxCycles) {
-            QFAIL("Maximum cycle count reached");
+
+        maxCyclesReached |= cycles >= s_maxCycles;
+        stop |= maxCyclesReached;
+        if (m_design->decode->opcode.uValue() == RVInstr::ECALL) {
+            unsigned status = m_design->registerFile->getRegister(s_ecallreg);
+            if (status == s_success) {
+                stop |= true;
+            } else if (status == s_fail) {
+                return "Test: '" + m_currentTest + "' failed: Internal test error.\n\t test number: " +
+                       QString::number(m_design->registerFile->getRegister(s_statusreg));
+            }
+        }
+    } while (!stop);
+
+    if (maxCyclesReached) {
+        return "Test: '" + m_currentTest + "' failed: Maximum cycle count reached\n\t test number: " +
+               QString::number(m_design->registerFile->getRegister(s_statusreg));
+    }
+
+    // Test successful
+    return QString();
+}
+
+void tst_RISCV::runTests() {
+    const auto dir = QDir(s_testdir);
+    const auto testFiles = dir.entryList({"*.s"});
+
+    for (const auto& test : testFiles) {
+        m_currentTest = test;
+
+        if (skipTest(m_currentTest))
+            continue;
+
+        // Compile test file
+        const auto binFile = compileTestFile(test);
+        if (binFile.isNull()) {
+            QString err = "Test: '" + test + "' failed: Could not compile test file.";
+            QFAIL(err.toStdString().c_str());
+        }
+
+        m_design = std::make_unique<SingleCycleRISCV>();
+        loadBinaryToSimulator(binFile);
+        m_design->verifyAndInitialize();
+
+        QString err = executeSimulator();
+        if (!err.isNull()) {
+            QFAIL(err.toStdString().c_str());
         }
     }
 }
