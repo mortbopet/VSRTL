6014f4778d97791d898aa5b8f8a839eee47172fd
Morten Borup Petersen <morten_bp@live.dk>
2020-01-06 13:48:48 +0100

    Move RISC-V Single cycle processor to Ripes

--- app.cpp
+++ app.cpp
@@ -9,7 +9,6 @@
 #include "vsrtl_mainwindow.h"
 
 #include "components/Leros/SingleCycleLeros/SingleCycleLeros.h"
-#include "components/RISC-V/riscv_ss.h"
 
 #include <chrono>
 
@@ -21,19 +20,7 @@ int main(int argc, char** argv) {
 
     Q_INIT_RESOURCE(icons);
 
-    vsrtl::RISCV::SingleCycleRISCV design;
-
-    /**
-    start:
-    li a0 0
-    li a1 10
-    loop:
-    addi a0 a0 1
-    bne a0 a1 loop
-    j start
-    */
-    std::vector<unsigned> program = {0x00000513, 0x00a00593, 0x00150513, 0xfeb51ee3, 0xff1ff06f};
-    design.instr_mem->addInitializationMemory(0x0, program.data(), program.size());
+    vsrtl::AdderAndReg design;
 
     vsrtl::MainWindow w(design);
 
