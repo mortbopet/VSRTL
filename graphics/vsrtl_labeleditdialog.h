7acb19efbc0a824eb716da97134466b8f817b8f5
Morten Borup Petersen <morten_bp@live.dk>
2020-01-26 20:08:02 +0100

    Disallow empty label text

--- graphics/vsrtl_labeleditdialog.h
+++ graphics/vsrtl_labeleditdialog.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <QDialog>
+#include <QDialogButtonBox>
+#include <QPushButton>
 
 #include "ui_vsrtl_labeleditdialog.h"
 
@@ -21,6 +23,10 @@ public:
     explicit LabelEditDialog(QWidget* parent = nullptr) : QDialog(parent), m_ui(new Ui::LabelEditDialog) {
         m_ui->setupUi(this);
         setWindowTitle("Edit label");
+
+        connect(m_ui->text, &QLineEdit::textChanged, [=](const QString& text) {
+            m_ui->buttonBox->button(QDialogButtonBox::Ok)->setEnabled(!text.isEmpty());
+        });
     }
     ~LabelEditDialog() { delete m_ui; }
 };
