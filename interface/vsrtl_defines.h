45237b8882ce10d1db6010c44da11752f94be133
Morten Borup Petersen <morten_bp@live.dk>
2021-06-16 15:31:25 +0200

    Add bitsize of base VSRTL type

--- interface/vsrtl_defines.h
+++ interface/vsrtl_defines.h
@@ -7,10 +7,12 @@
 namespace vsrtl {
 using VSRTL_VT_U = uint64_t;
 using VSRTL_VT_S = int64_t;
-static_assert(CHAR_BIT == 8, "");
+static_assert(CHAR_BIT == 8, "VSRTL only supports systems with CHAR_BIT = 8");
 static_assert(sizeof(VSRTL_VT_S) == sizeof(VSRTL_VT_U), "Base value types must be equal in size");
 static_assert(std::is_unsigned<VSRTL_VT_U>::value, "VSRTL_VT_U must be an unsigned data type");
 static_assert(std::is_signed<VSRTL_VT_S>::value, "VSRTL_VT_S must be a signed data type");
+
+constexpr VSRTL_VT_U VSRTL_VT_BITS = sizeof(VSRTL_VT_U) * CHAR_BIT;
 
 template <typename T>
 constexpr VSRTL_VT_U VT_U(const T& v) {
